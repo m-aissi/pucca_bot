017b245942f301ad9d52fa080e4da5f1c282ea68
Mehdi Aissi <mehdi.aissi@outlook.fr>
2025-07-01 21:07:59 +0200

    init commit needing only bg aniamtion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import TypeIt from "typeit";
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pucca_bot';
+
+
+  currentTime = new Date().toLocaleTimeString();
+
+  ngOnInit() {
+    setInterval(() => {
+      this.currentTime = new Date().toLocaleTimeString();
+    }, 1000);
+
+
+    new TypeIt("#element",{speed: 55})
+    .type("Salut c moi Pucca Crimson Tears, enchantée")
+    .delete()
+    .type("Plus a little more.")
+    .go();
+
+
+  }
+// rainbowtext
+  // new TypeIt("#callback", {
+  //   strings: ["Look, it's rainbow text!"],
+  //   afterStep: function (instance) {
+  //     instance.getElement().style.color = getRandomColor();
+  //   },
+  // }).go();
 }
