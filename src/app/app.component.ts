6d14d86cc50551446468d3449babc69ac045a11a
Mehdi Aissi <mehdi.aissi@outlook.fr>
2025-07-04 10:51:02 +0200

    fixed illisible text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -52,6 +52,12 @@ export class AppComponent {
           const mainContainer = document.getElementById('main-container');
           if (mainContainer) {
             mainContainer.style.backgroundColor = this.puccaToDisplay.backgroundColor;
+            let blackModeHours = [5,6,7,8,9,10,11,12,13,14,15];
+            if (blackModeHours.includes(heure)) {
+              mainContainer.style.color = 'black';
+            } else {
+              mainContainer.style.color = 'white';
+            }
           }
 
           
